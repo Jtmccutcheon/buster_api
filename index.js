18e9ff0f78fdc89240ad2ddf35ac5ff3dac7416c
unknown <jt.mccutcheon@live.com>
2021-12-09 23:52:50 -0500

    0.0.0.0

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ fastify.register(mercurius, {
 
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT);
+    await fastify.listen(process.env.PORT, '0.0.0.0');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
