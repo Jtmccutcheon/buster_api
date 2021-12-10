2c22a23961a0a7d9a36738db09a7bbf295f94f14
unknown <jt.mccutcheon@live.com>
2021-12-09 23:38:56 -0500

    try this

--- dbConnector.js
+++ dbConnector.js
@@ -4,16 +4,13 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const dbConnector = async (fastify, options, done) => {
-  try {
-    await mongoose.connect(process.env.MONGO_DB, {
+const dbConnector = () =>
+  mongoose
+    .connect(process.env.MONGO_DB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
-    done();
-  } catch (error) {
-    console.error(error);
-  }
-};
+    })
+    .then(i => i)
+    .catch(err => console.log(err));
 
 module.exports = fp(dbConnector);
