a932458654b4df15dea14bd9418f324dda4ba2d3
unknown <jt.mccutcheon@live.com>
2021-12-22 13:20:59 -0500

    add to whitelist

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const whitelist = [
   'http://localhost:3000',
   'https://busterq.herokuapp.com',
-  'https://busteranalytics-beta.netlify.app/',
+  'https://busteranalytics-beta.netlify.app',
 ];
 
 fastify.register(dbConnector);
